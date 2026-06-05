e7965bce8a1e3044fa9df643af2c74e4b5242e76
jisungin <jisungin0513@gmail.com>
2026-06-05 17:27:33 +0900

    [Feat] MinionsPlayerController HUD 데이터 바인딩 추가

--- Source/RefTarkov/Private/MinionsPlayerController.cpp
+++ Source/RefTarkov/Private/MinionsPlayerController.cpp
@@ -4,14 +4,20 @@
 #include "MinionsPlayerController.h"
 #include "Managers/MinionsCheatManager.h"
 #include "UObject/ConstructorHelpers.h"
+#include "MinionsGameModeBase.h"
+#include "MinionsPlayerState.h"
+#include "Components/RaidStatsComponent.h"
+#include "UI/W_HUD.h"
+#include "UI/W_RaidResult.h"
+#include "Characters/PlayerCharacter.h"
+#include "Components/HealthComponent.h"
+#include "Weapons/WeaponBase.h"
 
 AMinionsPlayerController::AMinionsPlayerController()
 {
 	bShowMouseCursor = true;
 	bEnableClickEvents = true;
 	bEnableMouseOverEvents = true;
-
-	
 
 	static ConstructorHelpers::FClassFinder<UCheatManager> CheatBPClass(TEXT("/Game/BP_MinionsCheatManager"));
 	if (CheatBPClass.Succeeded())
@@ -28,10 +34,96 @@ void AMinionsPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ÀÔ·Â ¸ğµå¸¦ Game And UI·Î ¼³Á¤ÇÏ¿© °ÔÀÓ Á¶ÀÛ°ú ¸¶¿ì½º Ä¿¼­ »ç¿ëÀ» µ¿½Ã¿¡ °¡´ÉÇÏ°Ô ÇÕ´Ï´Ù.
+	// ì…ë ¥ ëª¨ë“œë¥¼ Game And UIë¡œ ì„¤ì •í•˜ì—¬ ê²Œì„ ì¡°ì‘ê³¼ ë§ˆìš°ìŠ¤ ì»¤ì„œ ì‚¬ìš©ì„ ë™ì‹œì— ê°€ëŠ¥í•˜ê²Œ í•©ë‹ˆë‹¤.
 	FInputModeGameAndUI InputMode;
 	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputMode.SetHideCursorDuringCapture(false);
 	SetInputMode(InputMode);
 
+	if (HUDWidgetClass)
+	{
+		HUDWidget = CreateWidget<UW_HUD>(this, HUDWidgetClass);
+		if (HUDWidget)
+		{
+			HUDWidget->AddToViewport();
+		}
+	}
+
+	AMinionsGameModeBase* GM =
+		GetWorld() ? GetWorld()->GetAuthGameMode<AMinionsGameModeBase>() : nullptr;
+	if (GM)
+	{
+		GM->OnRaidEnded.AddDynamic(this, &AMinionsPlayerController::HandleRaidEnded);
+	}
+}
+
+void AMinionsPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	APlayerCharacter* PlayerChar = Cast<APlayerCharacter>(InPawn);
+	if (!PlayerChar) return;
+
+	if (UHealthComponent* HealthComp = PlayerChar->GetHealthComponent())
+	{
+		HealthComp->OnHealthChanged.AddDynamic(this, &AMinionsPlayerController::HandleHealthChanged);
+		if (HUDWidget)
+		{
+			HUDWidget->UpdateHealth(HealthComp->GetCurrentHealth(), HealthComp->GetMaxHealth());
+		}
+	}
+
+	PlayerChar->OnWeaponEquipped.AddDynamic(this, &AMinionsPlayerController::HandleWeaponEquipped);
+}
+
+void AMinionsPlayerController::HandleRaidEnded(APlayerCharacter* PlayerChar, ERaidEndReason EndReason)
+{
+	AMinionsPlayerState* PS = GetPlayerState<AMinionsPlayerState>();
+	if (PS && PS->RaidStats)
+	{
+		if (RaidResultWidgetClass)
+		{
+			UW_RaidResult* ResultWidget = CreateWidget<UW_RaidResult>(this, RaidResultWidgetClass);
+			if (ResultWidget)
+			{
+				ResultWidget->InitResult(PS->RaidStats->GetSnapshot());
+				ResultWidget->AddToViewport();
+			}
+		}
+	}
+
+	FInputModeUIOnly InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputMode.SetWidgetToFocus(nullptr);
+	SetInputMode(InputMode);
+	bShowMouseCursor = true;
+}
+
+void AMinionsPlayerController::HandleHealthChanged(UHealthComponent* HealthComp,
+	float OldHealth, float NewHealth, AController* InstigatedBy)
+{
+	if (HUDWidget && HealthComp)
+	{
+		HUDWidget->UpdateHealth(NewHealth, HealthComp->GetMaxHealth());
+	}
+}
+
+void AMinionsPlayerController::HandleWeaponEquipped(AWeaponBase* NewWeapon)
+{
+	if (!NewWeapon || !HUDWidget) return;
+
+	NewWeapon->OnAmmoChanged.AddDynamic(this, &AMinionsPlayerController::HandleAmmoChanged);
+
+	if (HUDWidget)
+	{
+		HUDWidget->UpdateAmmo(NewWeapon->GetCurrentAmmoInMag(), 0);
+	}
+}
+
+void AMinionsPlayerController::HandleAmmoChanged(int32 CurrentAmmo)
+{
+	if (HUDWidget)
+	{
+		HUDWidget->UpdateAmmo(CurrentAmmo, 0);
+	}
 }
--- Source/RefTarkov/Public/MinionsPlayerController.h
+++ Source/RefTarkov/Public/MinionsPlayerController.h
@@ -4,11 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Types/RaidTypes.h"
 #include "MinionsPlayerController.generated.h"
 
-/**
- * 
- */
+class UHealthComponent;
+class AWeaponBase;
+class APlayerCharacter;
+
 UCLASS()
 class REFTARKOV_API AMinionsPlayerController : public APlayerController
 {
@@ -19,4 +21,28 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UW_HUD> HUDWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UW_RaidResult> RaidResultWidgetClass;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UW_HUD> HUDWidget;
+
+	UFUNCTION()
+	void HandleRaidEnded(APlayerCharacter* PlayerChar, ERaidEndReason EndReason);
+
+	UFUNCTION()
+	void HandleHealthChanged(UHealthComponent* HealthComp,
+		float OldHealth, float NewHealth, AController* InstigatedBy);
+
+	UFUNCTION()
+	void HandleWeaponEquipped(AWeaponBase* NewWeapon);
+
+	UFUNCTION()
+	void HandleAmmoChanged(int32 CurrentAmmo);
 };
