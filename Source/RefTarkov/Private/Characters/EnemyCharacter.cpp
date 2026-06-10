87bd1c0275e7464304d1035ad0b589d19446d647
jisungin <jisungin0513@gmail.com>
2026-06-10 17:48:21 +0900

    [Feat] MeleeEnemy 피격 시 InvestigateLocation 설정, 사망 시 BT 즉시 정지

--- Source/RefTarkov/Private/Characters/EnemyCharacter.cpp
+++ Source/RefTarkov/Private/Characters/EnemyCharacter.cpp
@@ -1,41 +1,145 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Characters/EnemyCharacter.h"
 #include "AI/EnemyAIController.h"
+#include "BrainComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Components/SphereComponent.h"
+#include "Inventory/InventoryContainer.h"
+#include "Inventory/InventoryComponent.h"
+#include "Inventory/Items/ItemInstance.h"
+#include "Loot/LootTableDataAsset.h"
 #include "MinionsPlayerState.h"
 #include "Components/RaidStatsComponent.h"
-#include "Loot/LootContainerActor.h"
 
 AEnemyCharacter::AEnemyCharacter()
 {
 	AIControllerClass = AEnemyAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	TeamId = 1;
 
-	TeamId = 1; // 적 팀
+	CorpseDetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CorpseDetection"));
+	CorpseDetectionSphere->SetupAttachment(RootComponent);
+	CorpseDetectionSphere->SetSphereRadius(100.f);
+	CorpseDetectionSphere->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	CorpseDetectionSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AEnemyCharacter::HandleDeath(UHealthComponent* HealthComponent, AController* Killer)
 {
-	// [역할] 기반 클래스의 래그돌/파괴 처리를 먼저 실행한다.
-	//        그 다음, 킬러가 플레이어 컨트롤러일 때만 킬 카운트를 기록한다.
-	//        AI끼리 피해를 주고받을 때는 카운트되면 안 된다.
+	if (AAIController* AIC = Cast<AAIController>(GetController()))
+	{
+		AIC->StopMovement();
+		if (AIC->BrainComponent)
+		{
+			AIC->BrainComponent->StopLogic(TEXT("Dead"));
+		}
+		AIC->ClearFocus(EAIFocusPriority::Gameplay);
+	}
+
 	Super::HandleDeath(HealthComponent, Killer);
+	SetLifeSpan(120.f);
 
 	if (APlayerController* PC = Cast<APlayerController>(Killer))
 	{
 		if (AMinionsPlayerState* PS = PC->GetPlayerState<AMinionsPlayerState>())
 		{
 			PS->RaidStats->RecordKill();
-
-			if (CorpseClass)
-			{
-				FActorSpawnParameters Params;
-				Params.SpawnCollisionHandlingOverride =
-					ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-				GetWorld()->SpawnActor<ALootContainerActor>(CorpseClass, GetActorLocation(), FRotator::ZeroRotator, Params);
-			}
 		}
 	}
+
+	InitCorpseLoot();
+
+	CorpseDetectionSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CorpseDetectionSphere->UpdateOverlaps();
+}
+
+void AEnemyCharacter::InitCorpseLoot()
+{
+	CorpseLoot = NewObject<UInventoryContainer>(this);
+	CorpseLoot->Initialize(CorpseSlotCount, -1.f);
+
+	if (ULootTableDataAsset* Table = LootTable.LoadSynchronous())
+	{
+		Table->RollAndAddTo(CorpseLoot);
+	}
+}
+
+void AEnemyCharacter::Interact_Implementation(AActor* Interactor)
+{
+	if (bAlreadyLooted || !CorpseLoot || !Interactor)
+		return;
+
+	UInventoryComponent* PlayerInv = Interactor->FindComponentByClass<UInventoryComponent>();
+	if (!PlayerInv)
+		return;
+
+	const TArray<FItemInstance>& Items = CorpseLoot->GetSlots();
+	for (int32 i = 0; i < Items.Num(); ++i)
+	{
+		const FItemInstance& Item = Items[i];
+		if (Item.IsEmpty())
+			continue;
+
+		int32 Remainder = 0;
+		PlayerInv->TryAddItem(Item.GetItemDef(), Item.StackCount, Remainder);
+
+		const int32 Moved = Item.StackCount - Remainder;
+		if (Moved > 0)
+			CorpseLoot->RemoveItemAt(i, Moved);
+	}
+
+	bAlreadyLooted = true;
+	CorpseDetectionSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+
+float AEnemyCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	const float Damage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (!IsAlive())
+		return Damage;
+
+	AEnemyAIController* AIC = Cast<AEnemyAIController>(GetController());
+	UBlackboardComponent* BB = AIC ? AIC->GetBlackboardComponent() : nullptr;
+	if (!BB)
+		return Damage;
+
+	if (BB->GetValueAsObject(AEnemyAIController::TargetActorKey))
+		return Damage;
+
+	FVector SuspectLocation = FVector::ZeroVector;
+	if (DamageCauser)
+	{
+		SuspectLocation = DamageCauser->GetActorLocation();
+	}
+	else if (EventInstigator && EventInstigator->GetPawn())
+	{
+		SuspectLocation = EventInstigator->GetPawn()->GetActorLocation();
+	}
+
+	if (!SuspectLocation.IsZero())
+	{
+		BB->SetValueAsVector(AEnemyAIController::InvestigateLocationKey, SuspectLocation);
+	}
+
+	return Damage;
+}
+
+FText AEnemyCharacter::GetInteractPrompt_Implementation() const
+{
+	return bAlreadyLooted
+		? FText::FromString(TEXT("Already, Searched"))
+		: FText::FromString(TEXT("Search Body"));
+}
+
+void AEnemyCharacter::OnFocusGained_Implementation(AActor* Interactor)
+{
+
+}
+
+void AEnemyCharacter::OnFocusLost_Implementation(AActor* Interactor)
+{
 
 }
--- Source/RefTarkov/Public/Characters/EnemyCharacter.h
+++ Source/RefTarkov/Public/Characters/EnemyCharacter.h
@@ -4,29 +4,54 @@
 
 #include "CoreMinimal.h"
 #include "Characters/MinionCharacterBase.h"
+#include "Loot/Interactable.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "EnemyCharacter.generated.h"
 
-class ALootContainerActor;
+class USphereComponent;
+class UInventoryContainer;
+class ULootTableDataAsset;
 
 UCLASS(Abstract)
-class REFTARKOV_API AEnemyCharacter : public AMinionCharacterBase
+class REFTARKOV_API AEnemyCharacter : public AMinionCharacterBase, public IInteractable
 {
 	GENERATED_BODY()
-	
+
 public:
 	AEnemyCharacter();
 
 public:
 	UBehaviorTree* GetBehaviorTree() const { return BehaviorTreeAsset; }
 
+	virtual float TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
 protected:
 	virtual void HandleDeath(UHealthComponent* HealthComponent, AController* Killer) override;
+
+protected:
+	virtual void Interact_Implementation(AActor* Interactor) override;
+	virtual FText GetInteractPrompt_Implementation() const override;
+	virtual void OnFocusGained_Implementation(AActor* Interactor) override;
+	virtual void OnFocusLost_Implementation(AActor* Interactor) override;
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintreadOnly, Category = "AI")
 	TObjectPtr<UBehaviorTree> BehaviorTreeAsset;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Loot")
+	TObjectPtr<USphereComponent> CorpseDetectionSphere;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Loot")
-	TSubclassOf<ALootContainerActor> CorpseClass;
+	TSoftObjectPtr<ULootTableDataAsset> LootTable;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Loot", meta = (ClampMin = "1"))
+	int32 CorpseSlotCount = 8;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UInventoryContainer> CorpseLoot;
+
+	bool bAlreadyLooted = false;
+
+	void InitCorpseLoot();
 };
